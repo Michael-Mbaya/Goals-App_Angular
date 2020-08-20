b6f5bfb003b0d6cf90f6b0dce08f0437b5451d69
Michael-Mbaya <gikundamike@gmail.com>
2020-08-20 18:19:19 +0300

    first Angular App

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,28 @@
 import { Component } from '@angular/core';
+import { Goal } from './goal';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'goals';
+
+
+export class AppComponent{
+
 }
+
+        //1.html {{goal}}
+// export class AppComponent {
+//   // title = 'goals';
+//   goal = 'Watch Finding Nemo'
+// }
+
+        //2.html *ngFor list
+// export class AppComponent {
+//   goals:string[];
+
+//   constructor(){
+//     this.goals = ['Watch finding Nemo', 'Buy cookies', 'Get new phone case']
+//   } 
+// }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,15 +3,32 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { GoalComponent } from './goal/goal.component';
+import { GoalDetailComponent } from './goal-detail/goal-detail.component';
+import { StrikethroughDirective } from './strikethrough.directive';
+import { DateCountPipe } from './date-count.pipe';
+
+
+// import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { GoalFormComponent } from './goal-form/goal-form.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    GoalComponent,
+    GoalDetailComponent,
+    StrikethroughDirective,
+    DateCountPipe,
+    GoalFormComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    AppRoutingModule,
+    FormsModule
   ],
+
   providers: [],
   bootstrap: [AppComponent]
 })
